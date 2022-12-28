2f1d14cb0ed7bdd5b80450abccd83291e87b0a08
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-12-28 10:43:56 +0100

    Initial dotstat

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/sh
 
+# Add paths to system PATH.
 path_prepend "$HOME/.local/bin"
 
-export EDITOR=nvim
+# Check if neovim is installed, otherwise default to vim.
+if ! command -v nvim > /dev/null; then
+    export EDITOR=vim
+else
+    export EDITOR=nvim
+fi
+
+# Check for dotfiles updates once after reboot.
+HAS_CHECKED_DOTFILES_PATH="/tmp/.dotstat_done"
+if [ ! -f "$HAS_CHECKED_DOTFILES_PATH" ]; then 
+    dotstat
+    touch $HAS_CHECKED_DOTFILES_PATH
+fi
+
