5cd49295edcee21158fa9d62c3962879ef89d5ea
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-12-28 10:46:53 +0100

    Alias vim=nvim if nvim is installed.

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -9,9 +9,11 @@ alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
 alias more=less
 
-alias chrome=google-chrome-stable
-
 alias bat='bat --theme Nord'
 
 alias callgrind='valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes'
 alias cachegrind='valgrind --tool=cachegrind'
+
+if command -v nvim > /dev/null; then
+    alias vim='nvim'
+fi
