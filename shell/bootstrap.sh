278bd20bb21cd043f91344095ac107f7c4392195
Oliver Larsson <larsson.e.oliver@gmail.com>
2022-12-30 14:19:47 +0100

    Check dotfiles on startup and post notification.

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -10,3 +10,11 @@ else
     export EDITOR=nvim
 fi
 
+# Check for dotfiles updates once after reboot.
+HAS_CHECKED_DOTFILES_PATH="/tmp/.dotstat_done"
+if [ ! -f "$HAS_CHECKED_DOTFILES_PATH" ]; then 
+    (dotstat_dotfiles_check_and_notify &)
+    (dotstat_bootstrap_check_and_notify &)
+    touch $HAS_CHECKED_DOTFILES_PATH
+fi
+
--- shell/functions/dotstats.sh
+++ shell/functions/dotstats.sh
@@ -128,3 +128,149 @@ dotstat_bootstrap() {
         fi
     fi
 }
+
+dotstat_dotfiles_check_and_notify() {
+    # Check if notify-send is present on the system
+    if ! command -v notify-send > /dev/null 2>&1; then
+        return
+    fi
+
+    NOTIFYARGS="--app-name=dotstat"
+
+    ICONPATH="$HOME/.local/share/icons/dotfiles.png"
+    if ls "$ICONPATH" > /dev/null 2>&1; then
+        ICONARGS="--icon=$ICONPATH"
+    else
+        ICONARGS=""
+    fi
+
+    # Check if git is available
+    if ! prog_loc="$(type "git")" || [ -z "$prog_loc" ] || [ "$prog_loc" = "git not found" ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat" "Git could not be found."
+        return
+    fi
+
+    # Check for active internet connection
+    printf "%s\n\n" "GET http://google.com HTTP/1.0" | nc google.com 80 > /dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat" "Failed to get status of remote dotfiles. No active internet connection detected!"
+        return
+    fi
+
+    # Ensure a path to the local dotfiles repo has been provided
+    if [ -z ${LOCAL_DOTFILES_REPOSITORY+x} ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat" "Must set the 'LOCAL_DOTFILES_REPOSITORY' variable with path to your dotfiles directory!"
+        return
+    fi
+
+    # Ensure path to dotfiles repo has expanded tilde for home dir
+    # Not sure how portable this method is...
+    local_dotfiles_repo=$(eval "echo $LOCAL_DOTFILES_REPOSITORY")
+    cd "${local_dotfiles_repo}" || return
+
+    # Make sure the path we were given is a git repo
+    if [ ! -d ./.git ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat" "Provided dotfile directory is not a git repository!"
+        return
+    fi
+
+    # Get remote branch info
+    git fetch > /dev/null 2>&1
+
+    # Retrieve hashes from git
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @\{u\})
+    BASE=$(git merge-base @ @\{u\})
+
+    # Check for uncommitted changes
+    ! git diff-index --quiet HEAD --
+    UNCOMMITTED=$?
+
+    # Determine status of dotfiles
+    if [ -z "$LOCAL" ] || [ -z "$REMOTE" ] || [ -z "$BASE" ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat" "Failed to get status of local dotfiles!"
+    elif [ $UNCOMMITTED = 0 ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat" "Local dotfiles have uncommitted changes."
+    elif [ "$LOCAL" = "$REMOTE" ]; then
+        # Silent exit.
+        true
+    elif [ "$LOCAL" = "$BASE" ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat" "Remote dotfiles have changed."
+    elif [ "$REMOTE" = "$BASE" ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat" "Local dotfiles have changed."
+    else
+        notify-send $ICONARGS $NOTIFYARGS "dotstat" "WARNING: Local and remote dotfiles have diverged. You may experience merge conflicts!"
+    fi
+}
+
+dotstat_bootstrap_check_and_notify() {
+    # Check if notify-send is present on the system
+    if ! command -v notify-send > /dev/null 2>&1; then
+        return
+    fi
+
+    NOTIFYARGS="--app-name=dotstat"
+
+    ICONPATH="$HOME/.local/share/icons/dotfiles.png"
+    if ls "$ICONPATH" > /dev/null 2>&1; then
+        NOTIFYARGS="$ICONARGS $NOTIFYARGS -i $ICONPATH"
+    fi
+
+    # Check if git is available
+    if ! prog_loc="$(type "git")" || [ -z "$prog_loc" ] || [ "$prog_loc" = "git not found" ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat bootstrap" "Git could not be found."
+        return
+    fi
+
+    # Check for active internet connection
+    printf "%s\n\n" "GET http://google.com HTTP/1.0" | nc google.com 80 > /dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat bootstrap" "Failed to get status of remote bootstrap. No active internet connection detected!"
+        return
+    fi
+
+    # Ensure a path to the local bootstrap repo has been provided
+    if [ -z ${LOCAL_BOOTSTRAP_REPOSITORY+x} ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat bootstrap" "Must set the 'LOCAL_BOOTSTRAP_REPOSITORY' variable with path to your bootstrap directory!"
+        return
+    fi
+
+    # Ensure path to bootstrap repo has expanded tilde for home dir
+    # Not sure how portable this method is...
+    local_bootstrap_repo=$(eval "echo $LOCAL_BOOTSTRAP_REPOSITORY")
+    cd "${local_bootstrap_repo}" || return
+
+    # Make sure the path we were given is a git repo
+    if [ ! -d ./.git ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat bootstrap" "Provided dotfile directory is not a git repository!"
+        return
+    fi
+
+    # Get remote branch info
+    git fetch > /dev/null 2>&1
+
+    # Retrieve hashes from git
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @\{u\})
+    BASE=$(git merge-base @ @\{u\})
+
+    # Check for uncommitted changes
+    ! git diff-index --quiet HEAD --
+    UNCOMMITTED=$?
+
+    # Determine status of bootstrap
+    if [ -z "$LOCAL" ] || [ -z "$REMOTE" ] || [ -z "$BASE" ]; then
+        notify-send $ICONARGS $NOTIFYARGS -u critical "dotstat bootstrap" "Failed to get status of local bootstrap!"
+    elif [ $UNCOMMITTED = 0 ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat bootstrap" "Local bootstrap have uncommitted changes."
+    elif [ "$LOCAL" = "$REMOTE" ]; then
+        # Silent exit.
+        true
+    elif [ "$LOCAL" = "$BASE" ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat bootstrap" "Remote bootstrap have changed."
+    elif [ "$REMOTE" = "$BASE" ]; then
+        notify-send $ICONARGS $NOTIFYARGS "dotstat bootstrap" "Local bootstrap have changed."
+    else
+        notify-send $ICONARGS $NOTIFYARGS "dotstat bootstrap" "WARNING: Local and remote bootstrap have diverged. You may experience merge conflicts!"
+    fi
+}
