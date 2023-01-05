a5e2afb8cd90f1e3c83edf3e9161e8d577ed25a3
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-01-05 22:17:51 +0100

    Changed preinstall script

--- pre-install.sh
+++ pre-install.sh
@@ -3,9 +3,9 @@
 # Check if oh-my-zsh is installed
 OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
-  echo 'Installing oh-my-zsh'
+  # echo 'Installing oh-my-zsh'
   /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
-  echo 'Updating oh-my-zsh'
-  "$OMZDIR"/tools/upgrade.sh
+  # echo 'Updating oh-my-zsh'
+  sh "$OMZDIR"/tools/upgrade.sh
 fi
