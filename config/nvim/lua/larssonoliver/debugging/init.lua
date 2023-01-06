556dc636dca5907b4f8cbd744ef9493ead55c18e
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-01-06 11:25:26 +0100

    DAP readded, but scuffed

--- config/nvim/lua/larssonoliver/debugging/init.lua
+++ config/nvim/lua/larssonoliver/debugging/init.lua
@@ -2,9 +2,6 @@ local dap = require("dap")
 local dapui = require("dapui")
 local daptext = require("nvim-dap-virtual-text")
 
-local remap = require("larssonoliver.keymap")
-local nnoremap = remap.nnoremap
-
 daptext.setup({})
 
 dapui.setup({
@@ -53,7 +50,7 @@ dapui.setup({
                 "repl",
                 -- "console",
             },
-            size = 15, 
+            size = 15,
             position = "bottom",
         },
     },
@@ -88,43 +85,22 @@ dapui.setup({
     }
 })
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
-    if not vim.g.larssonoliver_tree_visible_before_dap_set then
-        vim.g.larssonoliver_tree_visible_before_dap = require("nvim-tree.view").is_visible()
-        vim.g.larssonoliver_tree_visible_before_dap_set = true
-    end
-
-    require("nvim-tree.api").tree.close()
-    dapui.open({})
-end
-
-local on_dap_exit = function()
-    dapui.close({})
-    if vim.g.larssonoliver_tree_visible_before_dap then
-        local api = require("nvim-tree.api")
-        api.tree.close()
-        require("nvim-tree.api").tree.toggle(false, true)
-    end
-    vim.g.larssonoliver_tree_visible_before_dap_set = false
-end
-
-dap.listeners.before.event_terminated["dapui_config"] = on_dap_exit
-dap.listeners.before.event_exited["dapui_config"] = on_dap_exit
+dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
 require("larssonoliver.debugging.node")
 require("larssonoliver.debugging.python")
 
--- nnoremap("<Home>", function() dapui.toggle(1) end)
--- nnoremap("<End>", function() dapui.toggle(2) end)
+vim.keymap.set("n", "<Home>", function() dapui.toggle(1) end)
+vim.keymap.set("n", "<End>", function() dapui.toggle(2) end)
 
-nnoremap("<leader><leader>", function() dap.terminate() end)
+vim.keymap.set("n", "<leader><leader>", function() dap.terminate() end)
 
-nnoremap("<F5>", function() dap.continue() end)
-nnoremap("<F10>", function() dap.step_over() end)
-nnoremap("<F11>", function() dap.step_into() end)
---nnoremap("<Left>", function()
---    dap.step_out()
---end)
-nnoremap("<Leader>b", function() dap.toggle_breakpoint() end)
-nnoremap("<Leader>B", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
-nnoremap("<leader>rc", function() dap.run_to_cursor() end)
+vim.keymap.set("n", "<F5>", function() dap.continue() end)
+vim.keymap.set("n", "<F10>", function() dap.step_over() end)
+vim.keymap.set("n", "<F11>", function() dap.step_into() end)
+
+vim.keymap.set("n", "<Leader>b", function() dap.toggle_breakpoint() end)
+vim.keymap.set("n", "<Leader>B", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+vim.keymap.set("n", "<leader>rc", function() dap.run_to_cursor() end)
--- config/nvim/lua/larssonoliver/packer.lua
+++ config/nvim/lua/larssonoliver/packer.lua
@@ -79,47 +79,21 @@ return require("packer").startup(function(use)
         }
     })
 
-    -- -- LSP
-    -- use("neovim/nvim-lspconfig")
+    -- Debugging
+    use("mfussenegger/nvim-dap")
+    use("rcarriga/nvim-dap-ui")
+    use("theHamsta/nvim-dap-virtual-text")
+    use({
+        "mxsdev/nvim-dap-vscode-js",
+        requires = { "mfussenegger/nvim-dap" }
+    })
 
-
-
-    -- -- Autocompletion
-    -- use("hrsh7th/cmp-nvim-lsp")
-    -- use("hrsh7th/cmp-buffer")
-    -- use("hrsh7th/nvim-cmp")
-    -- use("L3MON4D3/LuaSnip")
-    -- use("saadparwaiz1/cmp_luasnip")
-    -- use("onsails/lspkind-nvim")
-    -- use("tzachar/cmp-tabnine", {
-    --     run = "./install.sh",
-    --     requires = "hrsh7th/nvim-cmp",
-    -- })
-
-
-    -- -- Nvim-tree
-    -- use({
-    --     "nvim-tree/nvim-tree.lua",
-    --     requires = { "nvim-tree/nvim-web-devicons", opt = true },
-    -- })
-
-
-    -- -- Debugging
-    -- use("mfussenegger/nvim-dap")
-    -- use("rcarriga/nvim-dap-ui")
-    -- use("theHamsta/nvim-dap-virtual-text")
-    -- use({
-    --     "mxsdev/nvim-dap-vscode-js",
-    --     requires = { "mfussenegger/nvim-dap" }
-    -- })
-
-    -- -- DAP servers
-    -- use({
-    --     "microsoft/vscode-js-debug",
-    --     opt = true,
-    --     run = "npm install --legacy-peer-deps && npm run compile"
-    -- })
-
+    -- DAP servers
+    use({
+        "microsoft/vscode-js-debug",
+        opt = true,
+        run = "npm install --legacy-peer-deps && npm run compile"
+    })
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
