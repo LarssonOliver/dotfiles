6bad2f6c58b5179076dfef372df7a4d41adbc549
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-01-06 11:25:26 +0100

    DAP readded, but scuffed

--- config/nvim/lua/larssonoliver/init.lua
+++ config/nvim/lua/larssonoliver/init.lua
@@ -1,6 +1,6 @@
 require("larssonoliver.set")
 require("larssonoliver.packer")
 require("larssonoliver.keymap")
--- require("larssonoliver.debugging")
+require("larssonoliver.debugging")
 require("larssonoliver.command")
 
--- post-install.sh
+++ post-install.sh
@@ -5,3 +5,8 @@ git submodule update --init --recursive
 
 git clean -ffdx \
     zsh/plugins/
+
+if command -v nvim > /dev/null; then
+    echo "Running NeoVim PackerSync"
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+fi
