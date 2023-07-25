c2add3cf0cfe8ee9f688fe8d1aefd15f84ad1116
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-07-25 15:46:44 +0200

    Lsp Config update

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -6,21 +6,6 @@ lsp.ensure_installed({
     "lua_ls",
 })
 
--- Fix Undefined global "vim"
-lsp.configure("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" }
-            }
-        }
-    }
-})
-
-require("lspconfig").volar.setup({
-    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" }
-})
-
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -85,6 +70,58 @@ lsp.on_attach(function(client, bufnr)
         end
     end
 end)
+
+-- Custom lsp configs --
+
+-- Fix Undefined global "vim"
+require("lspconfig").lua_ls.setup({
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
+        }
+    }
+})
+
+require("lspconfig").volar.setup({
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" }
+})
+
+require("lspconfig").yamlls.setup({
+    settings = {
+        redhat = {
+            telemetry = {
+                enabled = false
+            }
+        },
+        schemaStore = {
+            enable = true,
+            url = "https://www.schemastore.org/api/json/catalog.json",
+        },
+        yaml = {
+            -- https://www.reddit.com/r/neovim/comments/ze9gbe/comment/iz59clw
+            schemas = {
+                kubernetes = "*.yaml",
+                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines.yml",
+                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+            },
+        }
+    }
+})
+
+-- End custom lsp configs --
 
 lsp.setup()
 
--- config/nvim/lua/larssonoliver/packer.lua
+++ config/nvim/lua/larssonoliver/packer.lua
@@ -80,7 +80,7 @@ return require("packer").startup(function(use)
         config = function()
             require("indent_blankline").setup({
                 show_current_context = true,
-                show_current_context_start = true
+                show_current_context_start = false
             })
         end
     })
