d6482115c21d075a544f36407504bd2400febdc4
Oliver Larsson <larsson.e.oliver@gmail.com>
2024-04-23 11:03:46 +0200

    Atuin configs

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -1,2 +1,8 @@
 # Bind ctrl+space to accept autosuggestion.
 bindkey '^ ' autosuggest-accept
+
+# Setup Atuin if it's installed.
+if command -v atuin &> /dev/null; then
+    eval "$(atuin init zsh)"
+    eval "$(atuin gen-completions --shell zsh)"
+fi
