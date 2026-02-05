6483c28913fda5b1a1a6f0650183cbdba934554f
Oliver Larsson <larsson.e.oliver@gmail.com>
2026-02-05 09:41:15 +0100

    Enable skim pdf viewer for vimtex on macos

--- config/nvim/after/plugin/vimtex.lua
+++ config/nvim/after/plugin/vimtex.lua
@@ -1,4 +1,10 @@
-vim.g.vimtex_view_method = "zathura"
+if vim.fn.has("mac") == 1 then
+  vim.g.vimtex_view_method = "skim"
+  vim.g.vimtex_view_skim_sync = 1
+  vim.g.vimtex_view_skim_activate = 1
+elseif vim.fn.has("unix") == 1 then
+  vim.g.vimtex_view_method = "zathura"
+end
 
 vim.g.vimtex_compiler_latexmk = {
     build_dir = "",
